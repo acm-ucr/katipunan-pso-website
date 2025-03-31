1ef7fe0f12e80b5a0e224632561410301661200c
aldriebarcena <aldrievbarcena@gmail.com>
2025-03-31 16:02:01 -0700

    Create example component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import Example from "@/components/Example";
+
 const Home = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Home
-    </div>
-  );
+  return <Example text1="Adjust" text2="these" text3="Props" />;
 };
 
 export default Home;
