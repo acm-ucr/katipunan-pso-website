b8fa2728bcd31e2064764e773c9db859fea6607d
Jonathan Le <jonathanle062303@gmail.com>
2025-04-01 18:56:38 -0700

    Changed text on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Example from "@/components/Example";
 
 const Home = () => {
-  return <Example text1="Adjust" text2="these" text3="Props" />;
+  return <Example text1="Changed text" text2="these" text3="Props" />;
 };
 
 export default Home;
