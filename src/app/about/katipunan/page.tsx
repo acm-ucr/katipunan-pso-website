21dcfa213dab8727f19d9dc06d64d2324dbc3a11
Isaiah Alcayde <isaiahalcayde@gmail.com>
2025-04-15 16:37:45 -0700

    Removed react-icon library and fixed navbar change

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -6,14 +6,16 @@ import WIKImg3 from "@/public/What Is Kati Img3.jpeg";
 const Katipunan = () => {
   return (
     <div className="flex h-screen">
-      <div className="w-1/2 p-8 flex flex-col justify-center font-inria-serif text-xl">
-        <span className="font-bold">KatipunanPSO</span>Katipunan PSO is a cultural organization at the University of California,
-        Riverside that aims to enrich the understanding and appreciation of Filipino culture.
-        Katipunan hosts a variety of eents such as general meetings, cultural showcases, socials, and much more.
-        These vents are intended to raise awareness about Filipinx culture and to promote and encourage a strong
+      <div className="font-inria-serif flex w-1/2 flex-col justify-center p-8 text-xl">
+        <span className="font-bold">KatipunanPSO</span>Katipunan PSO is a
+        cultural organization at the University of California, Riverside that
+        aims to enrich the understanding and appreciation of Filipino culture.
+        Katipunan hosts a variety of eents such as general meetings, cultural
+        showcases, socials, and much more. These vents are intended to raise
+        awareness about Filipinx culture and to promote and encourage a strong
         sense of community among Filipinx students and allies at UCR.
       </div>
-      <div className="w-1/2 p-8 flex flex-wrap items-center justify-center">
+      <div className="flex w-1/2 flex-wrap items-center justify-center p-8">
         <Image
           src={WIKImg1}
           width={200}
