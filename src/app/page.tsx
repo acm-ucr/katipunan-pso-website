ed7ee14f16020472e4b02588f8964fcf44f0864f
aldriebarcena <aldrievbarcena@gmail.com>
2025-04-17 14:40:19 -0700

    Uh idk what i changed here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-// uncomment this line and comment everything else if working on example component
-// import Example from "@/components/Example";
+import Example from "@/components/Example";
 
 const Home = () => {
-  // uncomment this line and comment everything else if working on example component
-  // return <Example text1="Adjust" text2="these" text3="Props" />;
-  <></>
+  return <Example text1="Adjust" text2="these" text3="Props" />;
 };
 
 export default Home;
