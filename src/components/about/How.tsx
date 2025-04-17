a427f044b9a9db82ab24b38feb57cc56714cfdfc
james <james@jamess-MacBook-Air.local>
2025-04-17 14:10:53 -0700

    Added border around image and fixed the p tags

--- src/components/about/How.tsx
+++ src/components/about/How.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 const How = () => {
   return (
     <div className="flex w-full justify-end p-4">
-      <div className="flex w-1/2 items-center justify-center">
+      <div className="flex h-103 w-3xl items-center justify-center border-4 border-blue-400">
         <Image
           src="/Katipunan Logo.png"
           alt="Katipunan Logo"
-          width={400}
-          height={400}
+          width={732}
+          height={412}
         />
       </div>
 
@@ -16,13 +16,12 @@ const How = () => {
         <p className="text-kpso-blue font-inria-serif text-center text-5xl">
           HOW TO JOIN
         </p>
-        <div className="font-inria-serif flex flex-col gap-8 p-8 text-center text-3xl">
-          <p>Interested in joining Katipunan? It's</p>
-          <p>easy!</p>
-          <p>Just venmo our Treasurer, Ava Boado,</p>
-          <p>the general member fee of $32. And</p>
-          <p>just like that, you're now an official</p>
-          <p>Katipunero!</p>
+        <div className="font-inria-serif flex flex-col gap-4 p-10 text-center text-3xl">
+          <p>Interested in joining Katipunan? It's easy! </p>
+          <p>
+            Just venmo our Treasurer, Ava Boado, the general member fee of $32.
+          </p>
+          <p>And just like that, you're now an official Katipunero!</p>
           <p>Ava's Venmo: @pompu</p>
         </div>
       </div>
