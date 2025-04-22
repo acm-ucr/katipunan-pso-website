7199dfc7ca5973c78240139573322f820dada7f9
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-04-22 14:49:43 -0700

    Fixed error

---
+++ src/data/suite.ts
@@ -0,0 +1,8 @@
+export const tags = [
+  { name: "RURAL" },
+  { name: "MARIA CLARA" },
+  { name: "CORDILLERA" },
+  { name: "MODERN" },
+  { name: "FILLIPINO MARTIAL ARTS" },
+  { name: "TINIKLING" },
+];
