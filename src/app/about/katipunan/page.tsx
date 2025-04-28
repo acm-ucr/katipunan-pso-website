4b9e196532bc79c5e9917caf210e294b3d3d7975
aldriebarcena <aldrievbarcena@gmail.com>
2025-04-28 15:19:22 -0700

    Refactor about/katipunan

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -1,13 +1,22 @@
+import Header from "@/components/Header";
 import What from "@/components/about/What";
 import Who from "@/components/about/Who";
 import How from "@/components/about/How";
+import HeaderImg from "@/public/HEADER.webp";
 
 const Katipunan = () => {
   return (
     <>
+      <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
+
+      <div className="mb-32"></div>
       <What />
-      <Who />
-      <How />
+      <div className="mb-16"></div>
+      <div className="mx-16">
+        <Who />
+        <div className="mb-32"></div>
+        <How />
+      </div>
     </>
   );
 };
