edda5c85a4019cd46b2e15f4e60bb016dbeff7a0
aldriebarcena <aldrievbarcena@gmail.com>
2025-04-29 11:50:28 -0700

    Darken blue

--- src/components/culture/Suite.tsx
+++ src/components/culture/Suite.tsx
@@ -11,7 +11,7 @@ const Suite = () => {
             key={index}
             className="relative h-88 w-88 border-6 border-white text-3xl text-white"
           >
-            <div className="absolute inset-0 z-10 bg-blue-800 opacity-70" />
+            <div className="absolute inset-0 z-10 bg-blue-900 opacity-70" />
 
             <div className="font-inria-serif relative z-20 flex h-full flex-col items-center justify-center text-center text-5xl">
               {name.split(" ").map((word, i) => (
