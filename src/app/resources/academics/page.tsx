3c2ff866bac009e1c62fb254609795234ebed4ca
aldriebarcena <aldrievbarcena@gmail.com>
2025-04-29 11:49:01 -0700

    Add chartutor to page

--- src/app/resources/academics/page.tsx
+++ src/app/resources/academics/page.tsx
@@ -1,13 +1,12 @@
 import HeaderImg from "@/public/HEADER.webp";
 import Header from "@/components/Header";
+import Chartutor from "@/components/resources/Chartutor";
 
 const Academics = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="ACADEMICS" />
-      <div className="flex h-screen w-screen items-center justify-center">
-        Academics
-      </div>
+      <Chartutor />
     </>
   );
 };
