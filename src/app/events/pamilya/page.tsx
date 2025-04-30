872d566190c9d233c9938f4e9e5ce22916b22fa5
aldriebarcena <aldrievbarcena@gmail.com>
2025-04-30 16:37:01 -0700

    Add EventInfo component to page

--- src/app/events/pamilya/page.tsx
+++ src/app/events/pamilya/page.tsx
@@ -1,12 +1,13 @@
 import HeaderImg from "@/public/HEADER.webp";
 import Header from "@/components/Header";
+import EventInfo from "@/components/events/EventInfo";
 
 const Pamilya = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="PAMILYA PROGRAM" />
       <div className="flex h-screen w-screen items-center justify-center">
-        Pamilya
+        <EventInfo />
       </div>
     </>
   );
