a4e78b9038de6df5538fd7db6b9b3b153a789777
Gabriella Reyno <gabriellareyno@ellas-macbook-pro.local>
2025-04-30 15:17:43 -0700

    move page to eventinfo

--- src/components/events/EventInfo.tsx
+++ src/components/events/EventInfo.tsx
@@ -1,5 +1,23 @@
 const EventInfo = () => {
-  return <></>;
+  return(
+    <div className="font-inria-serif flex w-screen flex-col items-center justify-center gap-4">
+      <div className="text-kpso-blue text-center text-6xl">
+        WHAT IS THE PAMILYA PROGRAM?
+      </div>
+      <div className="text-center text-2xl leading-[70px]">
+        KATIPUNAN prides itself on its robust mentorship program, designed to
+        empower and guide our students towards personal and professional growth.
+        The program pairs experienced mentors with eager mentees, fostering a
+        supportive relationship that navigates the challenges of university life
+        and beyond. Mentors, selected from diverse backgrounds, offer invaluable
+        advice, share their knowledge, and provide a safe space for mentees to
+        explore their passions and interests. Through this program, Katipunan
+        nurtures a sense of community, creating bonds that extend long after
+        graduation and ensuring every student has access to the resources they
+        need to excel.
+      </div>
+    </div>
+  ); 
 };
 
 export default EventInfo;
