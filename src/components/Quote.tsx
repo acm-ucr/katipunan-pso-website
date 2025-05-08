2d1466a9600a39004a15e124fbe6c189db154a4e
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-07 18:02:14 -0700

    fix what was causing gap on right side

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -4,7 +4,7 @@ import Sun from "@/public/Sun.svg";
 
 const Quote = ({ Phrase, Source }: { Phrase: string; Source: string }) => {
   return (
-    <div className="font-inria-serif flex flex-row bg-gradient-to-b from-transparent via-[#51578160] to-[#2A3993] overflow-x-hidden">
+    <div className="font-inria-serif flex flex-row bg-gradient-to-b from-transparent via-[#51578160] to-[#2A3993]">
       <div className="ml-30 flex self-auto pt-35 text-9xl text-white">❝</div>
       <div className="-mr-30 ml-auto flex flex-col items-center justify-center text-white">
         <div className="text-center text-4xl">{Phrase}</div>
