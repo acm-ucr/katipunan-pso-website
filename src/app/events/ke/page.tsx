456ea47cb58431ee8228f58a36610eb2f32a1bbd
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-12 22:17:04 -0700

    KE Page changes

--- src/app/events/ke/page.tsx
+++ src/app/events/ke/page.tsx
@@ -5,7 +5,6 @@ import Bluebg from "@/public/Bluebg.svg";
 import Image from "next/image";
 import BlueFlower from "@/public/floral/blueflower.webp";
 import BlueVine from "@/public/floral/blueshortvine.svg";
-import DuoFlowers from "@/public/floral/bluedualflowers.webp";
 
 const Ke = () => {
   return (
@@ -15,38 +14,32 @@ const Ke = () => {
         alt="Picture of Club"
         txt="Katipunan Experience (KE)"
       />
-      <div className="font-inria-serif overflow-x-hidden overflow-y-hidden">
+      <div className="font-inria-serif mt-30 overflow-x-hidden overflow-y-hidden">
         <Image
           src={BlueFlower}
           alt="Flower"
-          className="absolute z-[-10] mt-30 -ml-80 w-180 scale-x-[-1] scale-y-[-1]"
+          className="absolute z-[-10] mt-190 -ml-90 w-180 scale-x-[-1] scale-y-[-1]"
         />
 
         <Image
           src={BlueVine}
           alt="Vine"
-          className="mt absolute right-0 z-[-10] w-70 rotate-30"
+          className="absolute right-0 z-[-10] w-70 scale-y-[-1]"
         />
 
         <Image
           src={BlueVine}
           alt="Vine"
-          className="absolute right-0 z-[-10] mt-60 w-70"
-        />
-
-        <Image
-          src={DuoFlowers}
-          alt="Flowers"
-          className="absolute -right-150 z-[-10] mt-160 w-380 rotate-315"
+          className="absolute top-160 right-0 z-[-10] mt-60 w-70"
         />
 
         <div className="z-0 mr-[18%] ml-[18%] flex flex-col items-center">
           <Image src={Bluebg} alt="Bg" className="w-250" />
-          <div className="text-kpso-blue pt-10 text-center text-5xl">
+          <div className="text-kpso-blue mt-20 pt-10 text-center text-5xl">
             The Katipunan Experience (KE)
             <span className="text-2xl text-black">
-              is our annual high school conference here at UC Riverside. At
-              KE&lt; high school students will be surrounded with mentor college
+              is our annual high school conference here at UC Riverside. At KE,
+              high school students will be surrounded with mentor college
               students, inspirational speakers, and other high school students
               from all parts of Southern California. This day will be packed
               with a lot of group bonding, a multitude of workshops that range
@@ -54,7 +47,7 @@ const Ke = () => {
               activities for the high schoolers and college students
             </span>
           </div>
-          <div className="pt-40 text-2xl">
+          <div className="pt-40 text-center text-2xl">
             Contact our Outreach Director, Lovel Cruz, with any questions,
             comments or concerns.
           </div>
