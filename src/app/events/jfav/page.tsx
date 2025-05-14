5548c2ea2c6a822cfeaaf1afe30fb205ce214566
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-13 17:34:03 -0700

    Remove image, made responsive

--- src/app/events/jfav/page.tsx
+++ src/app/events/jfav/page.tsx
@@ -1,8 +1,6 @@
 import HeaderImg from "@/public/components/HEADER.webp";
 import Header from "@/components/Header";
 import Contact from "@/components/events/Contact";
-import OrangeBackground from "@/public/orangebackground.svg";
-import Image from "next/image";
 import Quote from "@/components/Quote";
 
 const Jfav = () => {
@@ -14,23 +12,15 @@ const Jfav = () => {
           alt="Picture of Club"
           txt="Justice for Filipino American Veterans (JFAV)"
         />
-        <Image
-          src={OrangeBackground}
-          alt="Mock Image"
-          width={852}
-          height={479}
-          className="z-10 mx-auto my-40"
-        />
-
-        <Quote
-          Phrase="We want to keep the torch alive."
-          Source="Arturo Garcia"
-        />
-        <p className="font-inria-serif mx-60 mt-40 text-center text-2xl">
+        <p className="font-inria-serif m-20 text-center text-lg md:text-xl xl:mx-60 xl:my-30 xl:text-2xl">
           JFAV is a national alliance of Filipino veterans organizations and
           other advocates — the march is meant to serve as a reminder to ensure
           the sacrifices made by Filipinos during World War II aren't forgotten.
         </p>
+        <Quote
+          Phrase="We want to keep the torch alive."
+          Source="Arturo Garcia"
+        />
         <Contact
           name="jfav_la"
           instagramLink="https://www.instagram.com/jfav_la/"
