08db6c0c7a66343d4ef581cdb9d328d99f68f94e
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-13 17:27:34 -0700

    Finish academic resources page + responsive

--- src/components/resources/AcademicResources.tsx
+++ src/components/resources/AcademicResources.tsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+import vine from "@/public/floral/orangelongvine.svg";
+import Link from "next/link";
+
 const AcademicResources = () => {
-  return <></>;
+  return (
+    <div className="font-inria-serif mx-20 flex h-auto flex-col items-center overflow-x-hidden pb-10 xl:mx-auto xl:w-[60%]">
+      <div className="text-kpso-red-primary mt-15 text-center text-4xl md:text-5xl">
+        Academic Resources
+      </div>
+      <div className="">
+        <Image
+          src={vine}
+          alt="Vine"
+          className="text-kpso-red-primary w-170 pt-5"
+        />
+        <div className="mt-5 flex justify-between xl:mt-20">
+          <p className="text-md text-center underline underline-offset-2 md:text-2xl xl:ml-10">
+            <Link
+              href="https://docs.google.com/document/d/17v4ZdDqVcdK_ZGKPc7n4e4FOjMcHFBnJ9yMUBY1xWxM/edit?tab=t.0"
+              target="_blank"
+            >
+              Career Building Resources
+            </Link>
+          </p>
+          <p className="text-md text-center underline underline-offset-2 md:text-2xl xl:mr-10">
+            <Link
+              href="https://docs.google.com/document/d/1pCSChoLsAphWCwMbpiMup-CmsBSJh-9nuGcsRctFz_U/edit?tab=t.0"
+              target="_blank"
+            >
+              ARC Resources
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AcademicResources;
--- src/components/resources/Chartutor.tsx
+++ src/components/resources/Chartutor.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import vine from "@/public/floral/orangelongvine.svg";
-import schedule from "@/public/orangebackground.svg";
+import Schedule from "@/public/resources/ChartutorFall2024.jpeg";
 
 const Chartutor = () => {
   return (
-    <div className="font-inria-serif mx-auto flex h-auto w-[60%] flex-col items-center pb-10">
-      <div className="text-kpso-red-primary mt-15 text-5xl">CHARTUTOR</div>
+    <div className="font-inria-serif mx-20 flex h-auto flex-col items-center overflow-x-hidden pb-10 xl:mx-auto xl:w-[60%]">
+      <div className="text-kpso-red-primary mt-15 text-4xl md:text-5xl">
+        CHARTUTOR
+      </div>
       <div className="">
         <Image
           src={vine}
@@ -13,22 +15,28 @@ const Chartutor = () => {
           className="text-kpso-red-primary w-170 pt-5"
         />
       </div>
-      <div className="flex flex-row gap-50 self-start pt-5 2xl:gap-100">
+      <div className="flex flex-row self-start pt-5 md:mx-10 md:gap-50 xl:mx-0 2xl:gap-100">
         <div className="flex flex-col">
-          <div className="text-kpso-red-primary text-3xl">WHEN?</div>
-          <ul className="ml-7 list-disc pt-7 text-xl">
+          <div className="text-kpso-red-primary text-center text-xl md:text-left md:text-3xl">
+            WHEN?
+          </div>
+          <ul className="text-md ml-7 list-disc pt-2 md:text-xl xl:pt-7">
             <li>Refer to the schedule below</li>
           </ul>
         </div>
         <div className="flex flex-col">
-          <div className="text-kpso-red-primary text-3xl">WHERE?</div>
-          <ul className="list-disc pt-7 pl-8 text-xl">
+          <div className="text-kpso-red-primary text-center text-xl md:text-left md:text-3xl">
+            WHERE?
+          </div>
+          <ul className="text-md list-disc pt-2 pl-8 md:text-xl xl:pt-7">
             <li>Refer to the schedule below</li>
           </ul>
         </div>
       </div>
-      <div className="text-kpso-red-primary self-start pt-7 text-3xl">WHY?</div>
-      <ul className="ml-7 list-disc self-start pt-7 text-xl leading-10">
+      <div className="text-kpso-red-primary self-start pt-7 text-center text-xl md:mx-10 md:text-left md:text-3xl xl:mx-0">
+        WHY?
+      </div>
+      <ul className="text-md list-disc self-start pt-2 leading-10 md:mx-17 md:text-xl xl:ml-7 xl:pt-7">
         <li>
           Why not? Study with your friends, meet new people, enjoy baked goods,
           and possibly become this week's{" "}
@@ -36,7 +44,7 @@ const Chartutor = () => {
         </li>
       </ul>
       <div>
-        <Image src={schedule} alt="schedule" className="w-auto pt-10" />
+        <Image src={Schedule} alt="schedule" className="w-auto pt-10" />
       </div>
     </div>
   );
