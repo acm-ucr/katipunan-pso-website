bc5bf5633c119a4d027c11b878b5528c0ecfb38f
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-15 20:08:47 -0700

    Adjust flower

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -14,43 +14,45 @@ const Katipunan = () => {
       <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
 
       <div className="relative w-full overflow-x-hidden overflow-y-hidden">
-        <Image
-          src={BlueDual}
-          alt="Decorative Dual Flower"
-          width={900}
-          height={900}
-          className="absolute -right-80 bottom-140 z-10"
-        />
-        <Image
-          src={Vine}
-          alt="Decorative Vine"
-          width={300}
-          height={300}
-          className="absolute right-70 bottom-190 z-10"
-        />
+        <div>
+          <Image
+            src={BlueDual}
+            alt="Decorative Dual Flower"
+            width={900}
+            height={900}
+            className="absolute -right-80 bottom-140 z-10"
+          />
+          <Image
+            src={Vine}
+            alt="Decorative Vine"
+            width={300}
+            height={300}
+            className="absolute right-80 bottom-194 z-10"
+          />
 
-        <Image
-          src={Blue}
-          alt="Blue Single Flower"
-          width={300}
-          height={300}
-          className="absolute top-40 right-10 z-10"
-        />
+          <Image
+            src={Blue}
+            alt="Blue Single Flower"
+            width={300}
+            height={300}
+            className="absolute top-40 right-10 z-10"
+          />
 
-        <Image
-          src={Vine}
-          alt="Decorative Vine"
-          width={200}
-          height={200}
-          className="absolute top-45 -right-25 z-10 scale-x-[-1] scale-y-[-1]"
-        />
-        <Image
-          src={Vine}
-          alt="Decorative Vine"
-          width={200}
-          height={200}
-          className="absolute top-25 -right-7 z-10 scale-x-[-1] scale-y-[-1] -rotate-45"
-        />
+          <Image
+            src={Vine}
+            alt="Decorative Vine"
+            width={200}
+            height={200}
+            className="absolute top-45 -right-25 z-10 scale-x-[-1] scale-y-[-1]"
+          />
+          <Image
+            src={Vine}
+            alt="Decorative Vine"
+            width={200}
+            height={200}
+            className="absolute top-25 -right-7 z-10 scale-x-[-1] scale-y-[-1] -rotate-45"
+          />
+        </div>
 
         <div className="mb-32"></div>
         <What />
