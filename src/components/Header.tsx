8dc8b9ef8b26dda84ceb5b08fa2584b6b3cf44ba
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-15 20:32:19 -0700

    Made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,12 @@ type photoProps = {
 
 const Header = ({ webp, alt, txt }: photoProps) => {
   return (
-    <div className="relative h-96 w-full">
+    <div className="relative h-50 w-full sm:h-60 md:h-70 lg:h-85 xl:h-96">
       <Image src={webp} alt={alt} fill={true} className="object-cover" />
 
       <div className="absolute inset-0 bg-gradient-to-r from-indigo-900 to-gray-100 opacity-81"></div>
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="font-inria-serif text-6xl tracking-wide text-white">
+        <h1 className="font-inria-serif mx-10 text-center text-3xl tracking-wide text-white sm:text-4xl md:text-5xl xl:text-6xl">
           {txt}
         </h1>
       </div>
