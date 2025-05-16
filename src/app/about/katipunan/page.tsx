4594bed055561478df08b85a521e661fe2b72d0c
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-15 20:07:08 -0700

    Add flowers to about page

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -3,19 +3,63 @@ import What from "@/components/about/What";
 import Who from "@/components/about/Who";
 import How from "@/components/about/How";
 import HeaderImg from "@/public/components/HEADER.webp";
+import BlueDual from "@/public/floral/bluedualflowers.webp";
+import Blue from "@/public/floral/blueflower.webp";
+import Vine from "@/public/floral/bluevine.png";
+import Image from "next/image";
 
 const Katipunan = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
 
-      <div className="mb-32"></div>
-      <What />
-      <div className="mb-30"></div>
+      <div className="relative w-full overflow-x-hidden overflow-y-hidden">
+        <Image
+          src={BlueDual}
+          alt="Decorative Dual Flower"
+          width={900}
+          height={900}
+          className="absolute -right-80 bottom-140 z-10"
+        />
+        <Image
+          src={Vine}
+          alt="Decorative Vine"
+          width={300}
+          height={300}
+          className="absolute right-70 bottom-190 z-10"
+        />
 
-      <div className="flex w-full flex-col items-center justify-center gap-40">
-        <Who />
-        <How />
+        <Image
+          src={Blue}
+          alt="Blue Single Flower"
+          width={300}
+          height={300}
+          className="absolute top-40 right-10 z-10"
+        />
+
+        <Image
+          src={Vine}
+          alt="Decorative Vine"
+          width={200}
+          height={200}
+          className="absolute top-45 -right-25 z-10 scale-x-[-1] scale-y-[-1]"
+        />
+        <Image
+          src={Vine}
+          alt="Decorative Vine"
+          width={200}
+          height={200}
+          className="absolute top-25 -right-7 z-10 scale-x-[-1] scale-y-[-1] -rotate-45"
+        />
+
+        <div className="mb-32"></div>
+        <What />
+        <div className="mb-30"></div>
+
+        <div className="flex w-full flex-col items-center justify-center gap-40">
+          <Who />
+          <How />
+        </div>
       </div>
     </>
   );
--- src/components/about/What.tsx
+++ src/components/about/What.tsx
@@ -5,7 +5,7 @@ import WIKImg3 from "@/public/about/What Is Kati Img3.jpeg";
 
 const What = () => {
   return (
-    <div className="flex">
+    <div className="relative flex w-full overflow-x-hidden">
       <div className="font-inria-serif flex w-1/2 flex-col justify-center p-8 text-xl">
         <p className="-mt-10 ml-40 max-w-md text-center text-2xl leading-11 tracking-wider">
           <span className="inline-block text-5xl text-blue-800">
@@ -19,26 +19,27 @@ const What = () => {
           a strong sense of community among Filipinx students and allies at UCR.
         </p>
       </div>
-      <div className="flex w-1/2 flex-col items-center justify-center pt-16">
+
+      <div className="flex w-1/2 flex-col items-center justify-center overflow-x-hidden overflow-y-hidden pt-16">
         <Image
           src={WIKImg1}
           width={420}
           height={320}
-          alt="Kati logo"
+          alt="Kati image 1"
           className="z-0 -translate-x-40 rounded-xl"
         />
         <Image
           src={WIKImg2}
           width={420}
           height={320}
-          alt="Kati logo"
+          alt="Kati image 2"
           className="-z-10 -translate-y-4 translate-x-10 rounded-xl"
         />
         <Image
           src={WIKImg3}
           width={420}
           height={320}
-          alt="Kati logo"
+          alt="Kati image 3"
           className="-z-20 -translate-x-30 -translate-y-8 rounded-xl"
         />
       </div>
