69bec093f6aad626bfa2cd65bd35762f68a6a00b
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-20 14:04:03 -0700

    Animate PAM page

--- src/components/events/EventInfo.tsx
+++ src/components/events/EventInfo.tsx
@@ -1,10 +1,25 @@
+"use client";
+import { motion } from "motion/react";
+
 const EventInfo = () => {
   return (
     <div className="font-inria-serif mt-10 flex flex-col items-center justify-center gap-4 px-20 xl:mt-40 xl:px-40">
-      <div className="text-kpso-blue mb-8 text-center text-4xl xl:text-6xl">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+        viewport={{ once: true }}
+        className="text-kpso-blue mb-8 text-center text-4xl xl:text-6xl"
+      >
         WHAT IS THE PAMILYA PROGRAM?
-      </div>
-      <div className="text-md text-center sm:text-xl sm:leading-[50px] xl:text-2xl xl:leading-[70px]">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        viewport={{ once: true }}
+        className="text-md text-center sm:text-xl sm:leading-[50px] xl:text-2xl xl:leading-[70px]"
+      >
         KATIPUNAN prides itself on its robust mentorship program, designed to
         empower and guide our students towards personal and professional growth.
         The program pairs experienced mentors with eager mentees, fostering a
@@ -15,7 +30,7 @@ const EventInfo = () => {
         nurtures a sense of community, creating bonds that extend long after
         graduation and ensuring every student has access to the resources they
         need to excel.
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/events/PamGrid.tsx
+++ src/components/events/PamGrid.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "motion/react";
+
 import Image from "next/image";
 import PAM1 from "@/public/pam/PAM_1.webp";
 import PAM2 from "@/public/pam/PAM_2.webp";
@@ -12,12 +15,16 @@ const PamGrid = () => {
   return (
     <div className="grid grid-cols-1 place-items-center gap-6 px-20 py-12 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
       {images.map((img, i) => (
-        <div
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, delay: (i % 3) * 0.2 }}
+          viewport={{ once: true }}
           key={i}
           className="relative aspect-square h-[300px] w-[300px] sm:h-[320px] sm:w-[320px] xl:h-[350px] xl:w-[350px]"
         >
           <Image src={img} alt={`PAM ${i + 1}`} fill className="object-cover" />
-        </div>
+        </motion.div>
       ))}
     </div>
   );
