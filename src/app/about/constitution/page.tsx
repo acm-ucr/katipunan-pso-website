f912496a23b6c873626f221c7a322f56f2ceca52
ACM UCR <fake@domain.com>
2025-05-21 03:39:06 +0000

    convert images to webp

--- src/app/about/constitution/page.tsx
+++ src/app/about/constitution/page.tsx
@@ -12,7 +12,7 @@ const Constitution = () => {
       <div className="font-inria-serif mb-20 flex w-screen flex-col text-xl">
         <div className="flex flex-col items-center">
           <div className="mt-10 flex underline">
-            Katipunan Pilipino Student Organization
+            webppunan Pilipino Student Organization
           </div>
           <div className="font-thin italic">September 28, 2024</div>
           <div>University of California, Riverside</div>
@@ -22,7 +22,7 @@ const Constitution = () => {
         <div className="mr-60 ml-60 pt-8">
           <div className="font-bold italic">Preamble</div>
           <div className="pt-4">
-            We, the members of Katipunan (Pilipino Student Organization) at the
+            We, the members of webppunan (Pilipino Student Organization) at the
             University of California, Riverside, in order to advance the
             awareness and development of both the Pilipino and Pilipino-American
             culture through cultural, educational, communal, and social means,
@@ -31,12 +31,12 @@ const Constitution = () => {
           </div>
           <div className="pt-10 font-semibold underline">ARTICLE I</div>
           <div className="font-light italic">
-            Katipunan Pilipino Student Organization
+            webppunan Pilipino Student Organization
           </div>
           <div className="pt-4">
-            The official name of this organization shall be Katipunan (Pilipino
+            The official name of this organization shall be webppunan (Pilipino
             Student Organization) at the University of California, Riverside and
-            may be referred to as Katipunan for the remainder of this
+            may be referred to as webppunan for the remainder of this
             Constitution.
           </div>
 
@@ -67,11 +67,11 @@ const Constitution = () => {
           </div>
           <div className="pt-4 italic">A. Eligibility for Membership</div>
           <div>
-            Katipunan is all-inclusive. Membership is open to students and
+            webppunan is all-inclusive. Membership is open to students and
             non-students regardless of race, ethnicity, color, national origin,
             sexual orientation, gender, religion, ability, age, school, and
             political affiliation. All who pay the designated yearly dues are
-            official Katipunan members and are encouraged to promote the
+            official webppunan members and are encouraged to promote the
             organization’s well-being and spirit.
           </div>
           <br />
@@ -97,10 +97,10 @@ const Constitution = () => {
           </div>
           <br />
           <div className="">
-            Members of Katipunan are expected to adhere strictly to a zero
+            Members of webppunan are expected to adhere strictly to a zero
             tolerance policy to all forms of sexual assault and sexual
             harassment. Any instances of sexual assault/sexual harassment that
-            is brought to the attention of the Katipunan cabinet shall be
+            is brought to the attention of the webppunan cabinet shall be
             investigated and will result in revokement of membership for the
             individual who has perpetrated the act. No refund shall be given for
             any membership dues paid. Necessary officials shall also be
@@ -147,10 +147,10 @@ const Constitution = () => {
             Historian(s), Public Relations Chairperson(s), Social Activities
             Chairperson(s), Academics Chairperson, Athletics Coordinator, Alumni
             Liaison, Outreach Director, PACN Coordinator, Apprentice Advisor,
-            and Katipunan Apprentices.
+            and webppunan Apprentices.
           </div>
           <div>
-            Katipunan Apprentices are not limited to the instruction of the
+            webppunan Apprentices are not limited to the instruction of the
             Apprentice Advisor.
           </div>
           <br />
@@ -199,7 +199,7 @@ const Constitution = () => {
           <div>Appointments: Cabinet Positions</div>
           <ul className="ml-6 list-decimal">
             <li>
-              Katipunan Apprentices shall be appointed by the Cabinet during the
+              webppunan Apprentices shall be appointed by the Cabinet during the
               Fall Quarter.
             </li>
             <li>
@@ -250,14 +250,14 @@ const Constitution = () => {
           </div>
           <div>The President's duties include:</div>
           <ul className="ml-8 list-disc">
-            <li>Shall be the official representative of Katipunan.</li>
+            <li>Shall be the official representative of webppunan.</li>
             <li>
               Shall preside over all meetings of the organization unless
               otherwise stated.{" "}
             </li>
             <li>
               May delegate responsibility to a chosen representative regarding
-              Katipunan concerns.
+              webppunan concerns.
             </li>
             <li>
               Shall be an authorized signature on all official organization
@@ -280,7 +280,7 @@ const Constitution = () => {
               the duties of the President in their absence.
             </li>
             <li>
-              Shall be the official representative for Katipunan in
+              Shall be the official representative for webppunan in
               intercollegiate functions and any other collegiate Pilipino
               associations.{" "}
             </li>
@@ -308,12 +308,12 @@ const Constitution = () => {
             </li>
             <li>
               Shall be an official representative in any intra-campus
-              coalitions, alliances, and/or councils that which Katipunan is a
+              coalitions, alliances, and/or councils that which webppunan is a
               member of and officially affiliated.
             </li>
             <li>
               Shall participate in APISAC meetings and events as a
-              representative of Katipunan.
+              representative of webppunan.
             </li>
             <li>
               Shall be responsible for the development and maintenance of
@@ -397,7 +397,7 @@ const Constitution = () => {
             </li>
             <li>
               Shall handle documentation of cabinet attendance regarding all
-              Katipunan events.
+              webppunan events.
             </li>
             <li>
               Attendance shall be overseen along with the executive cabinet.
@@ -431,7 +431,7 @@ const Constitution = () => {
               organize community-oriented events.
             </li>
             <li>
-              Shall educate and provide resources to the members of Katipunan on
+              Shall educate and provide resources to the members of webppunan on
               issues regarding the community on a local, national, and global
               level.
             </li>
@@ -514,7 +514,7 @@ const Constitution = () => {
             </li>
             <li>
               Shall, in conjunction with the Athletics Coordinator, and/or any
-              other relevant Cabinet Members, manage the Katipunan Discord.
+              other relevant Cabinet Members, manage the webppunan Discord.
             </li>
             <li>
               Infographic requests made by Cabinet Members must be submitted at
@@ -522,7 +522,7 @@ const Constitution = () => {
             </li>
             <li>
               In conjunction with the Treasurer, shall be responsible for the
-              creation and overseeing of Katipunan merchandise.
+              creation and overseeing of webppunan merchandise.
             </li>
           </ul>
           <div>
@@ -550,10 +550,10 @@ const Constitution = () => {
             </li>
             <li>
               Shall organize and oversee the Mentorship Program within
-              Katipunan.
+              webppunan.
             </li>
             <li>
-              Shall be responsible for managing the UCR Katipunan Social Chairs
+              Shall be responsible for managing the UCR webppunan Social Chairs
               social media (i.e. Instagram)
             </li>
           </ul>
@@ -603,7 +603,7 @@ const Constitution = () => {
             </li>
             <li>
               Shall serve as the representative in overseeing all
-              Katipunan-affiliated athletic events, e-sports events, and sports
+              webppunan-affiliated athletic events, e-sports events, and sports
               teams.
             </li>
             <li>
@@ -612,7 +612,7 @@ const Constitution = () => {
             </li>
             <li>
               Shall, in conjunction with the Public Relations Chairperson(s),
-              and/or any other relevant Cabinet Members, manage the Katipunan
+              and/or any other relevant Cabinet Members, manage the webppunan
               Discord.
             </li>
             <li>
@@ -651,7 +651,7 @@ const Constitution = () => {
               administered by the organization.
             </li>
             <li>
-              Shall be the chairperson of the annual Katipunan Experience.
+              Shall be the chairperson of the annual webppunan Experience.
             </li>
             <li>
               Shall prepare budget reports including, but not limited to, how
@@ -716,23 +716,23 @@ const Constitution = () => {
           <ul className="ml-8 list-disc">
             <li>
               Shall be responsible for overseeing and advising the duties and
-              responsibilities of Katipunan Apprentices.
+              responsibilities of webppunan Apprentices.
             </li>
             <li>
               Shall conduct the Apprentice Interview and selection process.
             </li>
             <li>
               Shall serve as the primary individual to support the leadership
-              development of the Katipunan apprentices within the Apprenticeship
+              development of the webppunan apprentices within the Apprenticeship
               program.
             </li>
             <li>
               Shall be responsible for facilitating one-on-one check-ins during
-              the Fall, Winter, and Spring Quarter with the Katipunan
+              the Fall, Winter, and Spring Quarter with the webppunan
               Apprentices.
             </li>
           </ul>
-          <div>The Katipunan Apprentices’ Responsibilities include:</div>
+          <div>The webppunan Apprentices’ Responsibilities include:</div>
           <ul className="ml-8 list-disc">
             <li>
               Shall be responsible to support and to assist other cabinet
--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -1,20 +1,20 @@
 import Header from "@/components/Header";
-import What from "@/components/about/What";
+import webp from "@/components/about/webp";
 import Who from "@/components/about/Who";
 import How from "@/components/about/How";
 import HeaderImg from "@/public/components/HEADER.webp";
 import BlueDual from "@/public/floral/bluedualflowers.webp";
 import Blue from "@/public/floral/blueflower.webp";
-import Vine from "@/public/floral/bluevine.png";
+import Vine from "@/public/floral/bluevine.webp";
 import Image from "next/image";
 
-const Katipunan = () => {
+const webppunan = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
 
       <div className="mb-32"></div>
-      <What />
+      <webp />
       <div className="mb-30"></div>
 
       <div className="flex w-full flex-col items-center justify-center gap-40">
@@ -62,7 +62,7 @@ const Katipunan = () => {
         </div>
 
         <div className="mb-32"></div>
-        <What />
+        <webp />
         <div className="mb-30"></div>
 
         <div className="flex w-full flex-col items-center justify-center gap-40">
@@ -74,4 +74,4 @@ const Katipunan = () => {
   );
 };
 
-export default Katipunan;
+export default webppunan;
