007de427ec8531ab5691fa889912824862c2b8f8
APerson09 <ethanhuang63@gmail.com>
2025-05-20 22:57:46 -0700

    Fixed Subscribe Component

--- src/app/media/katifit/page.tsx
+++ src/app/media/katifit/page.tsx
@@ -1,12 +1,13 @@
 import HeaderImg from "@/public/components/HEADER.webp";
 import Header from "@/components/Header";
+import Subscribe from "@/components/media/Subscribe";
 
 const Katifit = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="KATIFITS" />
-      <div className="item-center flex justify-center">
-        <KatifitGrid />
+      <div className="flex h-screen w-screen items-center justify-center">
+        Katifit
       </div>
       <div>
         <Subscribe NewsLink="https://www.google.com/" />
