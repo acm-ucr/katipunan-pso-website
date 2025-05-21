b0b0a400d86efb9862fd9492cb8478c8926068c2
ACM UCR <fake@domain.com>
2025-05-21 14:18:21 +0000

    convert images to webp

--- src/app/media/katifit/page.tsx
+++ src/app/media/katifit/page.tsx
@@ -1,16 +1,16 @@
 import HeaderImg from "@/public/components/HEADER.webp";
 import Header from "@/components/Header";
-import KatifitGrid from "@/components/media/KatifitGrid";
+import webpfitGrid from "@/components/media/webpfitGrid";
 
-const Katifit = () => {
+const webpfit = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="KATIFITS" />
       <div className="item-center flex justify-center">
-        <KatifitGrid />
+        <webpfitGrid />
       </div>
     </>
   );
 };
 
-export default Katifit;
+export default webpfit;
