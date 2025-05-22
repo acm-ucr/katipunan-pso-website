af5cc256b46ecbf5fb71215f74945fa87e27e604
Jonathan Le <jonathanle062303@gmail.com>
2025-05-22 14:27:28 -0700

    Create galleries

---
+++ src/data/Videos.ts
@@ -0,0 +1,40 @@
+const videoData = [
+  {
+    name: "UCR Katipunan PSO",
+    src: "https://www.youtube.com/embed/u63yBFpMHgY",
+  },
+  {
+    name: "Katipunan PSO",
+    src: "https://www.youtube.com/embed/QwkJqMJRGnI",
+  },
+  {
+    name: "Cabinet Reveal 23-34",
+    src: "https://www.youtube.com/embed/WysMntMR_Q8",
+  },
+  {
+    name: "KATI KARNIVAL FG38",
+    src: "https://www.youtube.com/embed/owUOkT1Sbgo",
+  },
+  {
+    name: "PACN 35 Board Reveal",
+    src: "https://www.youtube.com/embed/8o3sVfJzM8A",
+  },
+  {
+    name: "KATIPUNAN EXPERIENCE 2023",
+    src: "https://www.youtube.com/embed/ix-BsREjF2E",
+  },
+  {
+    name: "Katipunan '23-'24 Apprentice Reveal",
+    src: "https://www.youtube.com/embed/8ab-0HLlCBM",
+  },
+  {
+    name: "Katipunan Gobble Gobble 2022",
+    src: "https://www.youtube.com/embed/1U7Eo_3Ndfo",
+  },
+  {
+    name: "2022 UCR Fall Quarter With Katipunan",
+    src: "https://www.youtube.com/embed/Du_BE4Qqu8I",
+  },
+];
+
+export default videoData;
