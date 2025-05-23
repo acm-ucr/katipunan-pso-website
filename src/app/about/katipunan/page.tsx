35f7f5d6b586b017c8d8e37011870394eead2bc3
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 15:01:33 -0700

    Move flowers and spacing to components

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-import { motion } from "motion/react";
 import Header from "@/components/Header";
 import What from "@/components/about/What";
 import Who from "@/components/about/Who";
 import How from "@/components/about/How";
 import HeaderImg from "@/public/components/HEADER.webp";
-import BlueDual from "@/public/floral/bluedualflowers.webp";
-import Blue from "@/public/floral/blueflower.webp";
-import Vine from "@/public/floral/bluevine.png";
-import Image from "next/image";
+import KatipunanFlowers from "@/components/about/KatipunanFlowers";
 
 const Katipunan = () => {
   return (
@@ -16,55 +11,8 @@ const Katipunan = () => {
       <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
 
       <div className="relative w-full overflow-x-hidden overflow-y-hidden">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          className="hidden xl:block"
-        >
-          <Image
-            src={BlueDual}
-            alt="Decorative Dual Flower"
-            width={900}
-            height={900}
-            className="absolute -right-80 bottom-140 z-10"
-          />
-          <Image
-            src={Vine}
-            alt="Decorative Vine"
-            width={300}
-            height={300}
-            className="absolute right-80 bottom-194 z-10"
-          />
-
-          <Image
-            src={Blue}
-            alt="Blue Single Flower"
-            width={300}
-            height={300}
-            className="absolute top-40 right-10 z-10"
-          />
-
-          <Image
-            src={Vine}
-            alt="Decorative Vine"
-            width={200}
-            height={200}
-            className="absolute top-45 -right-25 z-10 scale-x-[-1] scale-y-[-1]"
-          />
-          <Image
-            src={Vine}
-            alt="Decorative Vine"
-            width={200}
-            height={200}
-            className="absolute top-25 -right-7 z-10 scale-x-[-1] scale-y-[-1] -rotate-45"
-          />
-        </motion.div>
-
-        <div className="mb-32"></div>
+        <KatipunanFlowers />
         <What />
-        <div className="mb-30"></div>
-
         <div className="flex w-full flex-col items-center justify-center gap-40">
           <Who />
           <How />
