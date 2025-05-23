bb4f1537711de1133d8c3dc5e693457d90669aa0
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 14:15:58 -0700

    Update text

--- src/app/culture/suite/pandanggo/page.tsx
+++ src/app/culture/suite/pandanggo/page.tsx
@@ -20,16 +20,16 @@ const Pandanggo = () => {
 
         <p className="font-inria-serif text-md mx-10 mt-15 inline-block justify-center text-center leading-12 md:text-lg lg:mx-30 lg:text-xl xl:mx-70">
           This popular dance of grace and balance comes from Lubang Island,
-          Mindoro in the Visayas region. The term
+          Mindoro int he Visayas region. The term
           <span className="mx-2 text-4xl text-blue-800 lg:text-5xl">
             PANDANGGO
           </span>
-          comes from the Spanish word *fandango*, which is a dance characterized
+          comes from the spanish word fandango, which is a dance characterized
           by lively steps and clapping that varies in rhythm in ¾ time. This
-          particular Pandanggo involves the presence of three *tinggoy*, or oil
+          particular Pandanggo involves the presence of three tinggoy, or oil
           lamps, balanced on the head and the back of each hand. After a good
           catch, fishermen of Lingayen would celebrate by drinking wine and by
-          dancing, swinging, and circling a lighted lamp.
+          dancing, swinging and circling a lighted lamp.
         </p>
 
         <div className="hidden xl:block">
