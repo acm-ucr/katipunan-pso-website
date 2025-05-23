aa830ed339f14e57cd62a045d5e81a535c8b996f
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 15:22:05 -0700

    Remove div

--- src/components/media/KatifitGrid.tsx
+++ src/components/media/KatifitGrid.tsx
@@ -3,15 +3,17 @@ import { Katifits } from "@/data/Katifits";
 
 const KatifitGrid = () => {
   return (
-    <div className="grid grid-cols-1 justify-center gap-10 p-10 sm:grid-cols-2 md:gap-20 md:p-15 lg:grid-cols-3 lg:gap-30 lg:p-25">
-      {Katifits.map((item, index) => (
-        <KatifitCard
-          key={index}
-          src={item.src}
-          alt={item.alt}
-          href={item.href}
-        />
-      ))}
+    <div className="item-center flex justify-center">
+      <div className="grid grid-cols-1 justify-center gap-10 p-10 sm:grid-cols-2 md:gap-20 md:p-15 lg:grid-cols-3 lg:gap-30 lg:p-25">
+        {Katifits.map((item, index) => (
+          <KatifitCard
+            key={index}
+            src={item.src}
+            alt={item.alt}
+            href={item.href}
+          />
+        ))}
+      </div>
     </div>
   );
 };
