0386ff17757e1cc58b89e1f6ffb0cb8ad8c447d5
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 13:52:04 -0700

    Fix responsiveness

--- src/app/events/ke/page.tsx
+++ src/app/events/ke/page.tsx
@@ -15,29 +15,29 @@ const Ke = () => {
         txt="Katipunan Experience (KE)"
       />
       <div className="font-inria-serif mt-30 overflow-x-hidden overflow-y-hidden">
-        <Image
-          src={BlueFlower}
-          alt="Flower"
-          className="absolute top-180 z-[-10] mt-190 -ml-45 w-90 scale-x-[-1] scale-y-[-1] lg:top-125 lg:-ml-90 lg:w-180"
-        />
-
-        <Image
-          src={BlueVine}
-          alt="Vine"
-          className="absolute top-95 right-0 z-[-10] w-25 scale-y-[-1] md:top-115 md:w-45 lg:top-137 lg:w-70"
-        />
-
-        <Image
-          src={BlueVine}
-          alt="Vine"
-          className="absolute top-65 right-0 z-[-10] mt-60 w-25 md:top-110 md:w-45 lg:top-160 lg:w-70"
-        />
+        <div className="hidden lg:block">
+          <Image
+            src={BlueFlower}
+            alt="Flower"
+            className="absolute top-180 z-[-10] mt-190 -ml-45 w-90 scale-x-[-1] scale-y-[-1] lg:top-125 lg:-ml-90 lg:w-180"
+          />
+          <Image
+            src={BlueVine}
+            alt="Vine"
+            className="absolute top-95 right-0 z-[-10] w-25 scale-y-[-1] md:top-115 md:w-45 lg:top-137 lg:w-70"
+          />
+          <Image
+            src={BlueVine}
+            alt="Vine"
+            className="absolute top-65 right-0 z-[-10] mt-60 w-25 md:top-110 md:w-45 lg:top-160 lg:w-70"
+          />
+        </div>
 
         <div className="z-0 mr-[18%] ml-[18%] flex flex-col items-center">
           <Image src={KE} alt="Bg" className="w-250" />
-          <div className="text-kpso-blue mt-20 pt-10 text-center text-5xl">
+          <div className="text-kpso-blue mt-20 pt-10 text-center text-3xl lg:text-5xl">
             The Katipunan Experience (KE)
-            <span className="text-2xl text-black">
+            <span className="lg::text-2xl text-lg text-black">
               is our annual high school conference here at UC Riverside. At KE,
               high school students will be surrounded with mentor college
               students, inspirational speakers, and other high school students
@@ -47,7 +47,7 @@ const Ke = () => {
               activities for the high schoolers and college students
             </span>
           </div>
-          <div className="pt-40 text-center text-2xl">
+          <div className="pt-40 text-center text-lg lg:text-2xl">
             Contact our Outreach Director, Lovel Cruz, with any questions,
             comments or concerns.
           </div>
