e75ed1e75ed24fb6edefe4e4113bb2ebd986060e
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 15:30:21 -0700

    Move main parts to component

--- src/app/about/katipunan/page.tsx
+++ src/app/about/katipunan/page.tsx
@@ -1,23 +1,12 @@
 import Header from "@/components/Header";
-import What from "@/components/about/What";
-import Who from "@/components/about/Who";
-import How from "@/components/about/How";
 import HeaderImg from "@/public/components/HEADER.webp";
-import KatipunanFlowers from "@/components/about/KatipunanFlowers";
+import KatipunanComponent from "@/components/about/Katipunan";
 
 const Katipunan = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="WHAT IS KATIPUNAN?" />
-
-      <div className="relative w-full overflow-x-hidden overflow-y-hidden">
-        <KatipunanFlowers />
-        <What />
-        <div className="flex w-full flex-col items-center justify-center gap-40">
-          <Who />
-          <How />
-        </div>
-      </div>
+      <KatipunanComponent />
     </>
   );
 };
