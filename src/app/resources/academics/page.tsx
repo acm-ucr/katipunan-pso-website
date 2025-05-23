bf45345d45b271b87424e4aac2b4a35c58197a78
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-23 15:35:41 -0700

    Combine components to one

--- src/app/resources/academics/page.tsx
+++ src/app/resources/academics/page.tsx
@@ -1,18 +1,12 @@
 import HeaderImg from "@/public/components/HEADER.webp";
 import Header from "@/components/Header";
-import Chartutor from "@/components/resources/Chartutor";
-import AcademicResources from "@/components/resources/AcademicResources";
-import AcademicsFlowers from "@/components/resources/AcademicsFlowers";
+import AcademicsComponent from "@/components/resources/Academics";
 
 const Academics = () => {
   return (
     <>
       <Header webp={HeaderImg} alt="Picture of Club" txt="ACADEMICS" />
-      <div className="relative w-full overflow-hidden">
-        <AcademicsFlowers />
-        <Chartutor />
-        <AcademicResources />
-      </div>
+      <AcademicsComponent />
     </>
   );
 };
