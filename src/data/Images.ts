3f03f222eeb1daa59a63fb32ce94f4c7d1fcbe47
aldriebarcena <aldrievbarcena@gmail.com>
2025-05-22 22:41:27 -0700

    Update gallery images

--- src/data/Images.ts
+++ src/data/Images.ts
@@ -1,35 +1,73 @@
-import PAM1 from "@/public/pam/PAM_1.webp";
-import PAM2 from "@/public/pam/PAM_2.webp";
-import PAM3 from "@/public/pam/PAM_3.webp";
-import PAM4 from "@/public/pam/PAM_4.webp";
-import PAM5 from "@/public/pam/PAM_5.webp";
-import PAM6 from "@/public/pam/PAM_6.webp";
+import IMG1065 from "@/public/media/IMG_1065.webp";
+import IMG1129 from "@/public/media/IMG_1129.webp";
+import IMG1162 from "@/public/media/IMG_1162.webp";
+import IMG1213 from "@/public/media/IMG_1213.webp";
+import IMG1235 from "@/public/media/IMG_1235.webp";
+import IMG2481 from "@/public/media/IMG_2481.webp";
+import IMG2495 from "@/public/media/IMG_2495.webp";
+import IMG2641 from "@/public/media/IMG_2641.webp";
+import IMG2659 from "@/public/media/IMG_2659.webp";
+import IMG3176 from "@/public/media/IMG_3176.webp";
+import IMG3179 from "@/public/media/IMG_3179.webp";
+import IMG3199 from "@/public/media/IMG_3199.webp";
+import IMG3216 from "@/public/media/IMG_3216.webp";
+import IMG3222 from "@/public/media/IMG_3222.webp";
+import IMG3230 from "@/public/media/IMG_3230.webp";
+import IMG3235 from "@/public/media/IMG_3235.webp";
+import IMG3240 from "@/public/media/IMG_3240.webp";
+import IMG3253 from "@/public/media/IMG_3253.webp";
+import IMG3317 from "@/public/media/IMG_3317.webp";
+import IMG3328 from "@/public/media/IMG_3328.webp";
+import IMG4282 from "@/public/media/IMG_4282.webp";
+import IMG4284 from "@/public/media/IMG_4284.webp";
+import IMG4906 from "@/public/media/IMG_4906.webp";
+import IMG4986 from "@/public/media/IMG_4986.webp";
+import IMG5063 from "@/public/media/IMG_5063.webp";
+import IMG5194 from "@/public/media/IMG_5194.webp";
+import IMG5630 from "@/public/media/IMG_5630.webp";
+import IMG5787 from "@/public/media/IMG_5787.webp";
+import IMG5796 from "@/public/media/IMG_5796.webp";
+import IMG9419 from "@/public/media/IMG_9419.webp";
+import IMG9473 from "@/public/media/IMG_9473.webp";
+import IMG9474 from "@/public/media/IMG_9474.webp";
+import IMG9631 from "@/public/media/IMG_9631.webp";
+import IMG9650 from "@/public/media/IMG_9650.webp";
 
 const imageData = [
-  {
-    name: "PAM_1",
-    src: PAM1,
-  },
-  {
-    name: "PAM_2",
-    src: PAM2,
-  },
-  {
-    name: "PAM_3",
-    src: PAM3,
-  },
-  {
-    name: "PAM_4",
-    src: PAM4,
-  },
-  {
-    name: "PAM_5",
-    src: PAM5,
-  },
-  {
-    name: "PAM_6",
-    src: PAM6,
-  },
+  { name: "IMG_1065", src: IMG1065 },
+  { name: "IMG_1129", src: IMG1129 },
+  { name: "IMG_1162", src: IMG1162 },
+  { name: "IMG_1213", src: IMG1213 },
+  { name: "IMG_1235", src: IMG1235 },
+  { name: "IMG_2481", src: IMG2481 },
+  { name: "IMG_2495", src: IMG2495 },
+  { name: "IMG_2641", src: IMG2641 },
+  { name: "IMG_2659", src: IMG2659 },
+  { name: "IMG_3176", src: IMG3176 },
+  { name: "IMG_3179", src: IMG3179 },
+  { name: "IMG_3199", src: IMG3199 },
+  { name: "IMG_3216", src: IMG3216 },
+  { name: "IMG_3222", src: IMG3222 },
+  { name: "IMG_3230", src: IMG3230 },
+  { name: "IMG_3235", src: IMG3235 },
+  { name: "IMG_3240", src: IMG3240 },
+  { name: "IMG_3253", src: IMG3253 },
+  { name: "IMG_3317", src: IMG3317 },
+  { name: "IMG_3328", src: IMG3328 },
+  { name: "IMG_4282", src: IMG4282 },
+  { name: "IMG_4284", src: IMG4284 },
+  { name: "IMG_4906", src: IMG4906 },
+  { name: "IMG_4986", src: IMG4986 },
+  { name: "IMG_5063", src: IMG5063 },
+  { name: "IMG_5194", src: IMG5194 },
+  { name: "IMG_5630", src: IMG5630 },
+  { name: "IMG_5787", src: IMG5787 },
+  { name: "IMG_5796", src: IMG5796 },
+  { name: "IMG_9419", src: IMG9419 },
+  { name: "IMG_9473", src: IMG9473 },
+  { name: "IMG_9474", src: IMG9474 },
+  { name: "IMG_9631", src: IMG9631 },
+  { name: "IMG_9650", src: IMG9650 },
 ];
 
 export default imageData;
