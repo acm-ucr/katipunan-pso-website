96725a39f77de76b732b3a0916f903be219b6000
Jeffrey <Jeffx2016@gmail.com>
2025-05-26 22:25:45 -0700

    animated culture/SuiteOverview

--- src/components/culture/Suite.tsx
+++ src/components/culture/Suite.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import { tags } from "@/data/Suites";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,7 +11,13 @@ const Suite = () => {
         <div className="item-center grid grid-cols-1 gap-32 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 xl:px-12">
           {tags.map(({ name, image, link }, index) => (
             <Link href={link} key={index}>
-              <div className="relative h-88 w-88 overflow-hidden border-6 border-white text-3xl text-white">
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: (index % 3) * 0.2 }}
+                viewport={{ once: true }}
+                className="relative h-88 w-88 overflow-hidden border-6 border-white text-3xl text-white"
+              >
                 <div className="absolute inset-0 z-10 bg-blue-900 opacity-70" />
 
                 <div className="font-inria-serif relative z-20 flex h-full flex-col items-center justify-center text-center text-5xl">
@@ -19,7 +27,7 @@ const Suite = () => {
                 </div>
 
                 <Image src={image} alt="Image" fill className="object-cover" />
-              </div>
+              </motion.div>
             </Link>
           ))}
         </div>
