033d82b6975a2d5a2fe54d4020a9a40d820f4379
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-05-31 21:43:28 -0700

    Added animations

--- src/app/events/ke/page.tsx
+++ src/app/events/ke/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import HeaderImg from "@/public/components/HEADER.webp";
 import Header from "@/components/Header";
 import Contact from "@/components/events/Contact";
@@ -5,6 +6,7 @@ import KE from "@/public/events/KE.webp";
 import Image from "next/image";
 import BlueFlower from "@/public/floral/blueflower.webp";
 import BlueVine from "@/public/floral/blueshortvine.svg";
+import { motion } from "motion/react";
 
 const Ke = () => {
   return (
@@ -15,27 +17,62 @@ const Ke = () => {
         txt="Katipunan Experience (KE)"
       />
       <div className="font-inria-serif mt-30 overflow-x-hidden overflow-y-hidden">
-        <Image
-          src={BlueFlower}
-          alt="Flower"
-          className="absolute top-180 z-[-10] mt-190 -ml-45 w-90 scale-x-[-1] scale-y-[-1] lg:top-125 lg:-ml-90 lg:w-180"
-        />
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.1 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={BlueFlower}
+            alt="Flower"
+            className="absolute top-180 z-[-10] mt-190 -ml-45 w-90 scale-x-[-1] scale-y-[-1] lg:top-125 lg:-ml-90 lg:w-180"
+          />
+        </motion.div>
 
-        <Image
-          src={BlueVine}
-          alt="Vine"
-          className="absolute top-95 right-0 z-[-10] w-25 scale-y-[-1] md:top-115 md:w-45 lg:top-137 lg:w-70"
-        />
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.2 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={BlueVine}
+            alt="Vine"
+            className="absolute top-95 right-0 z-[-10] w-25 scale-y-[-1] md:top-115 md:w-45 lg:top-137 lg:w-70"
+          />
+        </motion.div>
 
-        <Image
-          src={BlueVine}
-          alt="Vine"
-          className="absolute top-65 right-0 z-[-10] mt-60 w-25 md:top-110 md:w-45 lg:top-160 lg:w-70"
-        />
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.2 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={BlueVine}
+            alt="Vine"
+            className="absolute top-65 right-0 z-[-10] mt-60 w-25 md:top-110 md:w-45 lg:top-160 lg:w-70"
+          />
+        </motion.div>
 
         <div className="z-0 mr-[18%] ml-[18%] flex flex-col items-center">
-          <Image src={KE} alt="Bg" className="w-250" />
-          <div className="text-kpso-blue mt-20 pt-10 text-center text-5xl">
+          <motion.div
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.2 }}
+            viewport={{ once: true }}
+          >
+            <Image src={KE} alt="Bg" className="w-250" />
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+            viewport={{ once: true }}
+            className="text-kpso-blue mt-20 pt-10 text-center text-5xl"
+          >
             The Katipunan Experience (KE)
             <span className="text-2xl text-black">
               is our annual high school conference here at UC Riverside. At KE,
@@ -46,11 +83,17 @@ const Ke = () => {
               from Academics to Mental Health to Arts Expression, and fun
               activities for the high schoolers and college students
             </span>
-          </div>
-          <div className="pt-40 text-center text-2xl">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3 }}
+            viewport={{ once: true }}
+            className="pt-40 text-center text-2xl"
+          >
             Contact our Outreach Director, Lovel Cruz, with any questions,
             comments or concerns.
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="">
