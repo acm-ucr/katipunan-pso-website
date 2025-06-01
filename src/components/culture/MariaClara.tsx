31969d80fd93e7e607acbce912f487dea5b9203b
james <james@jamess-MacBook-Air.local>
2025-06-01 12:42:05 -0700

    Added animation to each suite

--- src/components/culture/MariaClara.tsx
+++ src/components/culture/MariaClara.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import Image from "next/image";
 import blueFlower from "@/public/floral/blueflower.webp";
 import blueShortVine from "@/public/floral/blueshortvine.svg";
@@ -6,15 +9,26 @@ import Maria from "@/public/suites/MariaClara.webp";
 const MariaClara = () => {
   return (
     <div className="relative overflow-hidden">
-      <div className="flex justify-center">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        className="flex justify-center"
+      >
         <Image
           src={Maria}
           alt="Maria Clara Image"
           className="mx-0 mt-10 h-60 w-auto rounded-md sm:h-80 md:h-100 lg:h-120 xl:mx-80 xl:mt-20 xl:h-130"
         />
-      </div>
+      </motion.div>
 
-      <p className="font-inria-serif text-md mx-10 mt-15 inline-block justify-center text-center leading-12 md:text-lg lg:mx-30 lg:text-xl xl:mx-70">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        viewport={{ once: true }}
+        className="font-inria-serif text-md mx-10 mt-15 inline-block justify-center text-center leading-12 md:text-lg lg:mx-30 lg:text-xl xl:mx-70"
+      >
         <span className="mr-2 text-4xl text-blue-800 lg:text-5xl">
           MARIA CLARA
         </span>{" "}
@@ -26,7 +40,7 @@ const MariaClara = () => {
         courtship, love, and flirtation. The dances were heavily influenced by
         colonization and still encapsulate the native flare and style of the
         Philippines.
-      </p>
+      </motion.div>
 
       <div className="hidden xl:block">
         <Image
--- src/components/culture/MartialArts.tsx
+++ src/components/culture/MartialArts.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import Image from "next/image";
 import blueFlower from "@/public/floral/blueflower.webp";
 import blueShortVine from "@/public/floral/blueshortvine.svg";
@@ -6,15 +9,27 @@ import FMA from "@/public/suites/FMA.webp";
 const MartialArts = () => {
   return (
     <div className="relative overflow-hidden">
-      <div className="flex justify-center">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        viewport={{ once: true }}
+        className="flex justify-center"
+      >
         <Image
           src={FMA}
           alt="FMA Image"
           className="mx-0 mt-10 h-60 w-auto rounded-md sm:h-80 md:h-100 lg:h-120 xl:mx-80 xl:mt-20 xl:h-130"
         />
-      </div>
+      </motion.div>
 
-      <p className="font-inria-serif text-md mx-10 mt-15 inline-block justify-center text-center leading-12 md:text-lg lg:mx-30 lg:text-xl xl:mx-70">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        viewport={{ once: true }}
+        className="font-inria-serif text-md mx-10 mt-15 inline-block justify-center text-center leading-12 md:text-lg lg:mx-30 lg:text-xl xl:mx-70"
+      >
         <span className="mr-2 text-4xl text-blue-800 lg:text-5xl">
           FILIPINO MARTIAL ARTS (FMA)
         </span>
@@ -25,7 +40,7 @@ const MartialArts = () => {
         objects as weapons. The three terms–arnis, eskrima, and kali–are often
         used interchangeably, although there are some distinctions in their
         historical and regional usage.
-      </p>
+      </motion.div>
 
       <div className="hidden xl:block">
         <Image
