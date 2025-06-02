6aa97d62dd6febc5336f8874edb7a3d67423bff8
Jonathan Le <jonathanle062303@gmail.com>
2025-06-02 15:50:47 -0700

    Add gallery animations

--- src/components/media/ImageGallery.tsx
+++ src/components/media/ImageGallery.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
 
@@ -17,12 +19,19 @@ const ImageGallery = ({ galleryData }: ImageGalleryProps) => {
         <div className="mt-4 flex w-full justify-center">
           <div className="grid w-11/12 grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
             {galleryData.map((item, index) => (
-              <Image
-                className="rounded-lg object-cover"
+              <motion.div
                 key={index}
-                src={item.src}
-                alt={item.name}
-              />
+                initial={{ opacity: 0, x: -20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.3, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  className="rounded-lg object-cover"
+                  src={item.src}
+                  alt={item.name}
+                />
+              </motion.div>
             ))}
           </div>
         </div>
