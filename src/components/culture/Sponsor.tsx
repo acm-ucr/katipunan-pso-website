ee3ccedfef8bca1abf888beadfd728b1443fb42d
aldriebarcena <aldrievbarcena@gmail.com>
2025-06-02 13:24:26 -0700

    Animate

--- src/components/culture/Sponsor.tsx
+++ src/components/culture/Sponsor.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Image from "next/image";
 import Seafood from "@/public/pacn/SeafoodCity.webp";
 
@@ -5,13 +7,28 @@ const Sponser = () => {
   return (
     <div>
       <div className="text-kpso-red-primary flex flex-col items-center justify-center p-10 text-center text-4xl sm:text-5xl lg:text-6xl">
-        Sponsored By
-        <Image
-          src={Seafood}
-          className="mt-10"
-          width={300}
-          alt="Seafood City Logo"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
+          viewport={{ once: true }}
+        >
+          Sponsored By
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, delay: 0.4 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={Seafood}
+            className="mt-10"
+            width={300}
+            alt="Seafood City Logo"
+          />
+        </motion.div>
       </div>
     </div>
   );
