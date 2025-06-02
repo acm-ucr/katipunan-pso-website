d6cef1f4f735630750bc74f53ef0c0ddb38804b3
Jonathan Le <jonathanle062303@gmail.com>
2025-06-02 16:10:56 -0700

    Add home landing image animation

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -48,11 +48,18 @@ const Landing = () => {
         University of California, Riverside
       </motion.h1>
       <div className="relative w-max">
-        <Image
-          src={Header}
-          alt="Group"
-          className="w-80 rounded-lg shadow-lg sm:w-120 md:w-150 lg:w-200 xl:w-250"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true }}
+        >
+          <Image
+            src={Header}
+            alt="Group"
+            className="w-80 rounded-lg shadow-lg sm:w-120 md:w-150 lg:w-200 xl:w-250"
+          />
+        </motion.div>
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
