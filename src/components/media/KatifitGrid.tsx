69cfdf3e41b500a3616b0d7bc92e4289abb55f9c
Jonathan Le <jonathanle062303@gmail.com>
2025-06-03 13:07:40 -0700

    Destructure and fix galleries

--- src/components/media/KatifitGrid.tsx
+++ src/components/media/KatifitGrid.tsx
@@ -5,13 +5,8 @@ const KatifitGrid = () => {
   return (
     <div className="item-center flex justify-center">
       <div className="grid grid-cols-1 justify-center gap-10 p-10 sm:grid-cols-2 md:gap-20 md:p-15 lg:grid-cols-3 lg:gap-30 lg:p-25">
-        {Katifits.map((item, index) => (
-          <KatifitCard
-            key={index}
-            src={item.src}
-            alt={item.alt}
-            href={item.href}
-          />
+        {Katifits.map(({ src, alt, href }, index) => (
+          <KatifitCard key={index} src={src} alt={alt} href={href} />
         ))}
       </div>
     </div>
