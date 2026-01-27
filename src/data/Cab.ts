5ebc2a1ccef13fa3091f303c96f78e3f64bad79c
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-01-26 20:01:50 -0800

    Formatting

--- src/data/Cab.ts
+++ src/data/Cab.ts
@@ -1,35 +1,31 @@
-import Joshua from "@/public/cabinet/President - Joshua Perez.jpg"
-import Jill from "@/public/cabinet/External Vice President - Jill Ruiz-Delacruz.jpg"
-import Conner from "@/public/cabinet/Internal Vice President - Conner Balbuena.jpg"
-import Isabel from "@/public/cabinet/Treasurer - Isabel Dela Rama.jpg"
-import Russell from "@/public/cabinet/Secretary - Russell Robes.jpg"
-import Zane from "@/public/cabinet/Cultural and Community Advocacy Chair - Zane Sabal.jpg"
-import Jonny from "@/public/cabinet/Historian - Jonny Ramirez.jpg"
-import Karena from "@/public/cabinet/Historian - Karena Pascual.jpg"
-import Ella from "@/public/cabinet/Public Relations - Ella Reyno.jpg"
-import Rianna from "@/public/cabinet/Public Relations - Rianna Granados.jpg"
-import JJ from "@/public/cabinet/Social Activities Chairperson - JJ Halili Dove.jpg"
-import Josh from "@/public/cabinet/Social Activities Chairperson - Josh Whitfield.jpg"
-import Patricia from "@/public/cabinet/Academic Chairperson - Patricia Delfin.jpg"
-import Zion from "@/public/cabinet/Alumni Liaison - Zion Abendano.jpg"
-import Allyson from "@/public/cabinet/Athletics Coordinator - Allyson Mendoza.jpg"
-import Kayla from "@/public/cabinet/Outreach Director - Kayla Vergel de Dios .jpg"
-import Klarisse from "@/public/cabinet/PACN Coordinator - Klarisse Allyssa Dayrit.jpg"
-import Sam from "@/public/cabinet/Apprentice Advisor - Sam Cayabyab.jpg"
-import Izzy from "@/public/cabinet/Apprentice - Izzy Pendon.jpg"
-import Jacob from "@/public/cabinet/Apprentice - Jacob Adriano.jpg"
-import Karla from "@/public/cabinet/Apprentice - Karla Araracap.jpg"
-import Nathan from "@/public/cabinet/Apprentice - Nathan Barber.jpg"
-import Noah from "@/public/cabinet/Apprentice - Noah Joson.jpg"
-import Noelle from "@/public/cabinet/Apprentice - Noelle Melo.jpg"
-import Sean from "@/public/cabinet/Apprentice - Sean Del Rosario.jpg"
-import Steven from "@/public/cabinet/Apprentice - Steven Castaneda.jpg"
-import Warendel from "@/public/cabinet/Apprentice - Warendel Narvacan.jpg"
-import { Rubik } from "next/font/google"
-
-
-
-
+import Joshua from "@/public/cabinet/President - Joshua Perez.jpg";
+import Jill from "@/public/cabinet/External Vice President - Jill Ruiz-Delacruz.jpg";
+import Conner from "@/public/cabinet/Internal Vice President - Conner Balbuena.jpg";
+import Isabel from "@/public/cabinet/Treasurer - Isabel Dela Rama.jpg";
+import Russell from "@/public/cabinet/Secretary - Russell Robes.jpg";
+import Zane from "@/public/cabinet/Cultural and Community Advocacy Chair - Zane Sabal.jpg";
+import Jonny from "@/public/cabinet/Historian - Jonny Ramirez.jpg";
+import Karena from "@/public/cabinet/Historian - Karena Pascual.jpg";
+import Ella from "@/public/cabinet/Public Relations - Ella Reyno.jpg";
+import Rianna from "@/public/cabinet/Public Relations - Rianna Granados.jpg";
+import JJ from "@/public/cabinet/Social Activities Chairperson - JJ Halili Dove.jpg";
+import Josh from "@/public/cabinet/Social Activities Chairperson - Josh Whitfield.jpg";
+import Patricia from "@/public/cabinet/Academic Chairperson - Patricia Delfin.jpg";
+import Zion from "@/public/cabinet/Alumni Liaison - Zion Abendano.jpg";
+import Allyson from "@/public/cabinet/Athletics Coordinator - Allyson Mendoza.jpg";
+import Kayla from "@/public/cabinet/Outreach Director - Kayla Vergel de Dios .jpg";
+import Klarisse from "@/public/cabinet/PACN Coordinator - Klarisse Allyssa Dayrit.jpg";
+import Sam from "@/public/cabinet/Apprentice Advisor - Sam Cayabyab.jpg";
+import Izzy from "@/public/cabinet/Apprentice - Izzy Pendon.jpg";
+import Jacob from "@/public/cabinet/Apprentice - Jacob Adriano.jpg";
+import Karla from "@/public/cabinet/Apprentice - Karla Araracap.jpg";
+import Nathan from "@/public/cabinet/Apprentice - Nathan Barber.jpg";
+import Noah from "@/public/cabinet/Apprentice - Noah Joson.jpg";
+import Noelle from "@/public/cabinet/Apprentice - Noelle Melo.jpg";
+import Sean from "@/public/cabinet/Apprentice - Sean Del Rosario.jpg";
+import Steven from "@/public/cabinet/Apprentice - Steven Castaneda.jpg";
+import Warendel from "@/public/cabinet/Apprentice - Warendel Narvacan.jpg";
+import { Rubik } from "next/font/google";
 
 export const cabData = [
   {
@@ -193,5 +189,5 @@ export const cabData = [
     name: "Warendel Narvacan",
     webp: Warendel,
     is_apprentice: true,
-  }
+  },
 ];
